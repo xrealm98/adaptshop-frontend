3c528d0dca08e1d379c5fc15f00868b09adc097e
Xavier Real <xrealm@uoc.edu>
2026-04-23 15:15:37 +0200

    netlify conf

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: '',
+  apiUrl: 'https://adaptshop-backend.onrender.com/api',
   stripePublicKey:
     'pk_test_51TOIgl1AzSe6p9QWS33AQAUUpk32EBEH9SqAUj3zDk4EnpPm5j1LBiex2GgL3GDzlKs5cAhXlLRJbkOo6yuW5gZi002PhvVFSc',
 };
