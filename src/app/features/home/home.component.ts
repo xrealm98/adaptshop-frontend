ef50afd28dea74b1d044e2c30bd118ff0e99c245
Xavier Real <xrealm@uoc.edu>
2026-05-22 13:51:36 +0200

    Modified banner

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -33,7 +33,7 @@ export class HomeComponent {
   homeBanners = computed(() => {
     const settings = this.settingsService.settings();
     if (!settings) return null;
-    return settings.home?.banners;
+    return settings.home?.banners || [];
   });
 
   ngOnInit() {
